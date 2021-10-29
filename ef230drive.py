bb681c18fd0fa75acf897bd2ab5bd55c77591c70
Amy Biegalski <abiegals@utk.edu>
2021-10-29 13:25:50 -0400

    Update ef230drive.py

--- ef230drive.py
+++ ef230drive.py
@@ -2,6 +2,7 @@
 # Python Example for Drive, set LED, and set LCD
 # Fall EF230
 # Dr Amy Biegalski
+import syssys.path.append('/home/pi/sphero-sdk-raspberrypi-python/' )
 import sys
 import os
 # allow pull files from two layers above and append path

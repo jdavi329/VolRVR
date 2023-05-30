b795d5e3910ffc5bc00ccf727043334826c59ed6
Amy Biegalski <abiegals@utk.edu>
2023-05-30 13:09:00 -0400

    Update servoTest.py

--- servoTest.py
+++ servoTest.py
@@ -1,3 +1,8 @@
+# A.Biegalski
+# May 30 2023 
+# Problem:  Test Servos on Pi Servo Hat
+# Solution: use move_servo_position command.  
+# After any pan or tilt motion, rotate in opposite direction to reset servo position for the next user.
 import pi_servo_hat
 import time
 
@@ -12,14 +17,14 @@ test.restart()
 # Test Run
 #########################################
 # look left and right  Channel 1
-# sweep left 30 degrees, right 60, left 30
-for i in range(0,30):
+# sweep left 20 degrees, right 40, left 20
+for i in range(0,20):
     test.move_servo_position(1, i, 90)
     print("Position: ", end = '')
     print(test.get_servo_position(0,90))
-for i in range(30, -30, -1):
+for i in range(20, -20, -1):
     test.move_servo_position(1, i, 90)
-for i in range(0,30):
+for i in range(0,20):
     test.move_servo_position(1, i, 90)
 
 # look up and down  Channel 0
@@ -29,14 +34,14 @@ for i in range(0, 30):
     test.move_servo_position(0, i, 90)
 
 
-# write 30 degrees
-# test.move_servo_position(2, 30)
+# Tilt up 15 degrees, channel 0
+# test.move_servo_position(0, 15)
 
 # Pause 1 sec
 time.sleep (1)
 
-# Moves servo position to 85 degrees, Channel 1
-# test.move_servo_position(1, -85)
+# Pan servo position 15 degrees right, Channel 1
+# test.move_servo_position(1, -15)
 
 # Pause 1 sec
 time.sleep (1)

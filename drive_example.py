83a42eae660c85a5c3d5161ff032c3756b1c3d26
Amy Biegalski <abiegals@utk.edu>
2023-10-24 13:25:27 -0400

    Update drive_example.py

--- drive_example.py
+++ drive_example.py
@@ -1,4 +1,4 @@
-# A.Biegalski Oct 2020
+# A.Biegalski Aug 2021
 # For raspberry pi controlled Sphero RVR
 # Driving command examples:  straight, drive with heading, and turning 
 # For hardware specifications refer to github readme at https://github.com/abiegals/VolRVR/
